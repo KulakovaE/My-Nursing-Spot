2c884d6c560a8cf6099d6b37565481639708d1ed
Elena Kulakova <egeorgievska89@gmail.com>
2019-12-18 11:06:28 +0100

    Initial Commit

---
+++ myNursingSpot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myNursingSpot
+//
+//  Created by Elena Kulakova on 2019-12-18.
+//  Copyright © 2019 Elena Kulakova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
